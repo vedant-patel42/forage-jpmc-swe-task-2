6e77abd31ed67044813aae305225678dcecf4260
Vedant <vedant@Vedants-Air.lan>
2024-05-08 18:03:23 -0500

    Task-2 done

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -52,11 +52,12 @@ class Graph extends Component<IProps, {}> {
       elem.setAttribute('column-pivots', '["stock"]');
       elem.setAttribute('row-pivots', '["timestamp"]');
       elem.setAttribute('columns', '["top-ask-price"]');
-      elem.setAttribute('aggregates',
-        `{"stock":"distinct count",
-        "top_ask_price":"avg",
-        "top_bid_price":"avg",
-        "timestamp":"distinct count",`);
+      elem.setAttribute('aggregates', JSON.stringify({
+        stock:'distinctcount',
+        top_ask_price:'avg',
+        top_bid_price:'avg',
+        timestamp:'distinct count',
+      }));
     }
   }
 
